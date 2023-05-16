bb2ef70502217391bf263ec5afe9915b23244cbc
Jorge Zupirolli <jorgebertolo@gmail.com>
2023-05-16 09:29:09 -0400

    refactor: function azulRoutes

--- src/routes/azul.ts
+++ src/routes/azul.ts
@@ -11,10 +11,6 @@ interface AzulSearchRequest {
 }
 
 export async function azulRoutes(app: FastifyInstance) {
-    // app.get('/', async () => {
-    //     return {azul: true}
-    // })
-
     app.post('/search', async (request) => {
         const {departure, arrival, date}: AzulSearchRequest = request.body;
 
@@ -43,27 +39,48 @@ export async function azulRoutes(app: FastifyInstance) {
 
         const bearerToken = await getBearerTokenAzul();
 
-        const data = await getAzulFlights(bodyRequest, bearerToken);
+        const {trips: [travel]} = await getAzulFlights(bodyRequest, bearerToken);
 
-        if (data === undefined) return {message: "No flights found for this date."}
+        if (travel.journeys === null) return {message: "No flights found for this date."}
 
-        if (data.trips !== undefined && data.trips.length > 0) {
-            if (data.trips[0].flightType === null) {
-                return {message: "No flights found for this date."}
-            } else {
-                const {lowestPoints, highestPoints} = data.trips[0].fareInformation;
+        const flightData = {
+            qtdOfFlights: travel.journeys.length,
+            flights: []
+        };
 
-                return {
-                    departureStation: data.trips[0].departureStation,
-                    arrivalStation: data.trips[0].arrivalStation,
-                    departureDate: moment(data.trips[0].std).format("DD/MM/YYYY"),
-                    lowestPoints: lowestPoints,
-                    highestPoints: highestPoints,
-                    searchDate: moment().format("DD/MM/YYYY HH:mm:ss")
-                };
+        const journeys = travel.journeys;
+        for (let i = 0; i < journeys.length; i++) {
+            const journey = journeys[i];
+            const fares = journey.fares[0];
+            const paxPoints = fares.paxPoints[0];
+            const levels = paxPoints.levels[0];
+
+            const flight = {
+                value: levels.points,
+                connection: journey.identifier.connections?.count,
+                duration: `${journey.identifier.duration.hours}:${journey.identifier.duration.minutes}`,
+                airportConnections: journey.identifier.connections?.count > 0 ? [] : undefined
+            };
+
+            if (flight.connection > 0) {
+                const stationsInformation = journey.identifier.connections.stationsInformation;
+                for (let j = 0; j < stationsInformation.length; j++) {
+                    flight.airportConnections.push(stationsInformation[j].code);
+                }
             }
+
+            flightData.flights.push(flight);
         }
 
-        return {message: "No flights found for this date."};
+        return {
+            departureStation: travel.departureStation,
+            arrivalStation: travel.arrivalStation,
+            departureDate: moment(travel.std).format("DD/MM/YYYY"),
+            lowestPoints: travel.fareInformation.lowestPoints,
+            highestPoints: travel.fareInformation.highestPoints,
+            searchDate: moment().format("DD/MM/YYYY HH:mm:ss"),
+            flightData: flightData
+        };
+
     })
 }
