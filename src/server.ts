67a0b293ccc565ab28cb38ef367e6b060353228e
Jorge Zupirolli <jorgebertolo@gmail.com>
2023-05-20 17:37:30 -0400

    chore: added cookies

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,24 @@
-import fastify from "fastify";
-import {env} from "./env";
-import {azulRoutes} from "./routes/azul";
-import {latamRoutes} from "./routes/latam";
-import {interlineRoutes} from "./routes/interline";
-import {smilesRoutes} from "./routes/smiles";
+import fastify from 'fastify'
+import { env } from './env'
+import { azulRoutes } from './routes/azul'
+import { latamRoutes } from './routes/latam'
+import { interlineRoutes } from './routes/interline'
+import { smilesRoutes } from './routes/smiles'
+import cookie from '@fastify/cookie'
 
-const app = fastify();
+const app = fastify()
+
+app.register(cookie)
 
 app.get('/', async () => {
-    return {hello: 'world'};
+  return { hello: 'world' }
 })
 
-app.register(azulRoutes, {prefix: '/azul'});
-app.register(interlineRoutes, {prefix: '/interline'});
-app.register(latamRoutes, {prefix: '/latam'});
-app.register(smilesRoutes, {prefix: '/smiles'});
+app.register(azulRoutes, { prefix: '/azul' })
+app.register(interlineRoutes, { prefix: '/interline' })
+app.register(latamRoutes, { prefix: '/latam' })
+app.register(smilesRoutes, { prefix: '/smiles' })
 
-
-app.listen({port: env.PORT}).then(() => {
-  console.log(`Server is listening on port ${env.PORT}`);
-});
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`Server is listening on port ${env.PORT}`)
+})
