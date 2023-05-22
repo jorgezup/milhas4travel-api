3659e4493a5187c5c921b0cdc4c7676690e4af30
Jorge Zupirolli <jorgebertolo@gmail.com>
2023-05-21 22:26:58 -0400

    chore: added database connection

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { latamRoutes } from './routes/latam'
 import { interlineRoutes } from './routes/interline'
 import { smilesRoutes } from './routes/smiles'
 import cookie from '@fastify/cookie'
+import connectToDatabase from './db'
 
 const app = fastify()
 
@@ -19,6 +20,16 @@ app.register(interlineRoutes, { prefix: '/interline' })
 app.register(latamRoutes, { prefix: '/latam' })
 app.register(smilesRoutes, { prefix: '/smiles' })
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log(`Server is listening on port ${env.PORT}`)
-})
+connectToDatabase()
+  .then(() => {
+    console.log('Connected to database')
+    app.listen({ port: env.PORT }).then(() => {
+      console.log(`Server is listening on port ${env.PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log('Connection with database generated an error:\r\n')
+    console.error(error)
+    console.log('\r\nServer initialization cancelled')
+    process.exit(0)
+  })
