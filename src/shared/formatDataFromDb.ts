1db208bce4f1f61219c47baacb9fabeba21ed584
Jorge Zupirolli <jorgebertolo@gmail.com>
2023-05-23 23:20:40 -0400

    refactor: to be able to receive an array

--- src/shared/formatDataFromDb.ts
+++ src/shared/formatDataFromDb.ts
@@ -1,30 +1,31 @@
-const formatDataFromDb = (flight: any) => {
-  const {
-    searchId,
-    options,
-    departureStation,
-    arrivalStation,
-    departureDate,
-    lowestPoints,
-    highestPoints,
-    searchDate,
-  } = flight[0]
-  const { qtdOfFlights, flights } = flight[0].flightData
+const formatDataFromDb = (flights: any[]) => {
+  return flights.map((flight: any) => {
+    const {
+      searchId,
+      options,
+      departureStation,
+      arrivalStation,
+      departureDate,
+      lowestPoints,
+      highestPoints,
+      searchDate,
+      flightData: { qtdOfFlights },
+    } = flight
 
-  const formattedFlights = flights.map((flightData: any) => {
-    const { value, connection, duration, airportConnections } = flightData
+    const flights = flight.flightData.flights.map((flight: any) => {
+      const { value, connection, duration, airportConnections } = flight
+
+      return {
+        value,
+        connection,
+        duration,
+        airportConnections,
+      }
+    })
+
     return {
-      value,
-      connection,
-      duration,
-      airportConnections,
-    }
-  })
-
-  return {
-    searchId,
-    options,
-    data: {
+      searchId,
+      options,
       departureStation,
       arrivalStation,
       departureDate,
@@ -33,10 +34,10 @@ const formatDataFromDb = (flight: any) => {
       searchDate,
       flightData: {
         qtdOfFlights,
-        flights: formattedFlights,
+        flights,
       },
-    },
-  }
+    }
+  })
 }
 
 export default formatDataFromDb
