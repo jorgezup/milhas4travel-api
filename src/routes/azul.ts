42bbd16029eeaa603104816214298c0f8010e383
Jorge Zupirolli <jorgebertolo@gmail.com>
2023-05-27 17:32:32 -0400

    chore: added some new routes

--- src/routes/azul.ts
+++ src/routes/azul.ts
@@ -3,75 +3,166 @@ import { FastifyInstance } from 'fastify'
 import { getDatesArray } from '../shared/getDatesArray'
 import { AzulSearchRequest } from '../interfaces/flightDataInterface'
 import AzulFlight from '../models/AzulFlightModel'
-import { randomUUID } from 'crypto'
-import getCookie from '../shared/getCookie'
 import formatDataFromDb from '../shared/formatDataFromDb'
 import processSearchRequest from '../shared/processSearchRequest'
+import { randomUUID } from 'crypto'
+import { checkSessionIdExistsMiddleware } from '../middleware/check-session-id-exists'
 
 export async function azulRoutes(app: FastifyInstance) {
-  app.post('/search', async (request, reply) => {
-    try {
-      const { departuresStations, arrivalsStations, dateStart, dateEnd } =
-        request.body as AzulSearchRequest
+  app.post(
+    '/search',
+    {
+      preHandler: [checkSessionIdExistsMiddleware],
+    },
+    async (request, reply) => {
+      try {
+        const { departuresStations, arrivalsStations, dateStart, dateEnd } =
+          request.body as AzulSearchRequest
 
-      const datesArray = dateEnd
-        ? getDatesArray(dateStart, dateEnd)
-        : [dateStart]
+        const sessionId = request.cookies.sessionId as string
 
-      const searchId = randomUUID()
-      const sessionId = getCookie(request, reply)
+        const datesArray = dateEnd
+          ? getDatesArray(dateStart, dateEnd)
+          : [dateStart]
 
-      await processSearchRequest(
-        departuresStations,
-        arrivalsStations,
-        datesArray,
-        searchId,
-        sessionId,
-      )
+        const searchId = randomUUID()
+        // const sessionId = getCookie(request, reply)
 
-      return reply.status(200).send({
-        message:
-          'In a few minutes you will be able to see the results in the search page.',
-      })
-    } catch (error) {
-      // @ts-ignore
-      return reply.status(500).send({ message: error.message })
-    }
-  })
+        await processSearchRequest(
+          departuresStations,
+          arrivalsStations,
+          datesArray,
+          searchId,
+          sessionId,
+        )
 
-  app.get('/cheapest', async (request, reply) => {
-    try {
-      const { arrivalStation } = request.query as { arrivalStation: string }
+        return reply.status(200).send({
+          message:
+            "In a few minutes you're gonna get an email with dates and prices.",
+          searchId,
+        })
+      } catch (error) {
+        // @ts-ignore
+        return reply.status(500).send({ message: error.message })
+      }
+    },
+  )
 
-      const flights = await AzulFlight.find({
-        arrivalStation,
-      })
-        .sort({ lowestPoints: 1 })
-        .limit(10)
+  app.post(
+    '/schedule-flight-search',
+    {
+      preHandler: [checkSessionIdExistsMiddleware],
+    },
+    async (request, reply) => {
+      try {
+        const { departuresStations, arrivalsStations, dateStart, dateEnd } =
+          request.body as AzulSearchRequest
 
-      const formattedData = formatDataFromDb(flights)
+        const sessionId = request.cookies.sessionId as string
 
-      return reply.status(200).send(formattedData)
-    } catch (error) {
-      // @ts-ignore
-      return reply.status(500).send({ message: error.message })
-    }
-  })
-  // get flights by searchId
-  app.get('/search/:searchId', async (request, reply) => {
-    try {
-      const { searchId } = request.params as { searchId: string }
+        const datesArray = dateEnd
+          ? getDatesArray(dateStart, dateEnd)
+          : [dateStart]
 
-      const flights = await AzulFlight.find({
-        searchId,
-      })
+        const searchId = randomUUID()
 
-      const formattedData = formatDataFromDb(flights)
+        await processSearchRequest(
+          departuresStations,
+          arrivalsStations,
+          datesArray,
+          searchId,
+          sessionId,
+          true,
+        )
 
-      return reply.status(200).send(formattedData)
-    } catch (error) {
-      // @ts-ignore
-      return reply.status(500).send({ message: error.message })
-    }
-  })
+        return reply.status(200).send({
+          message:
+            'Every two hours during 7 days you are gonna get an email with dates and prices.',
+        })
+      } catch (error) {
+        // @ts-ignore
+        return reply.status(500).send({ message: error.message })
+      }
+    },
+  )
+
+  app.get(
+    '/cheapest',
+    {
+      preHandler: [checkSessionIdExistsMiddleware],
+    },
+    async (request, reply) => {
+      try {
+        const { arrivalStation } = request.query as { arrivalStation: string }
+
+        const sessionId = request.cookies.sessionId as string
+
+        const flights = await AzulFlight.aggregate([
+          { $match: { arrivalStation, sessionId } },
+          { $sort: { lowestPoints: 1, departureDate: 1 } },
+          { $group: { _id: '$departureDate', flight: { $first: '$$ROOT' } } },
+          { $replaceRoot: { newRoot: '$flight' } },
+          { $sort: { lowestPoints: 1 } },
+          { $limit: 10 },
+        ])
+
+        const formattedData = formatDataFromDb(flights)
+
+        return reply.status(200).send(formattedData)
+      } catch (error) {
+        // @ts-ignore
+        return reply.status(500).send({ message: error.message })
+      }
+    },
+  )
+
+  app.get(
+    '/search/:searchId',
+    {
+      preHandler: [checkSessionIdExistsMiddleware],
+    },
+    async (request, reply) => {
+      try {
+        const { searchId } = request.params as { searchId: string }
+
+        const sessionId = request.cookies.sessionId as string
+
+        const flights = await AzulFlight.find({
+          searchId,
+          sessionId,
+        })
+
+        const formattedData = formatDataFromDb(flights)
+
+        return reply.status(200).send(formattedData)
+      } catch (error) {
+        // @ts-ignore
+        return reply.status(500).send({ message: error.message })
+      }
+    },
+  )
+
+  // route my searches
+  app.get(
+    '/my-searches',
+    {
+      preHandler: [checkSessionIdExistsMiddleware],
+    },
+    async (request, reply) => {
+      try {
+        const sessionId = request.cookies.sessionId as string
+
+        const flights = await AzulFlight.find({
+          sessionId,
+        })
+
+        const formattedData = formatDataFromDb(flights)
+
+        return reply.status(200).send(formattedData)
+      } catch (error) {
+        // @ts-ignore
+        return reply.status(500).send({ message: error.message })
+      }
+    },
+  )
 }
